129b95c6e32535831bcb709992fd33c8ba259eef
Hyochan <hyochan02@gmail.com>
2026-01-07 15:07:49 +0900

    chore: 모달 관련 상태관리 추가 및 테스트용 모달 퍼블리싱

--- src/pages/homes/HomePage.tsx
+++ src/pages/homes/HomePage.tsx
@@ -1,5 +1,26 @@
+import useBaseModal from "@/stores/modals/baseModal";
+
 const HomePage = () => {
-  return <></>;
+  const { openModal } = useBaseModal();
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="flex gap-2">
+        <button
+          onClick={() => openModal("centerModal", { position: "center" })}
+          className="px-4 py-2 bg-primary-900 text-white rounded"
+        >
+          중앙 모달
+        </button>
+
+        <button
+          onClick={() => openModal("bottomModal", { position: "bottom" })}
+          className="px-4 py-2 bg-sub-900 text-white rounded"
+        >
+          하단 모달
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
