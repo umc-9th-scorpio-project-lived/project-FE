6c2439d0461b3554ab32187c04a0100829f2909e
OCJune <cjbormk11@gmail.com>
2026-01-15 10:36:29 +0900

    refactor: 마이페이지 레이아웃 개선

--- src/pages/users/UserPage.tsx
+++ src/pages/users/UserPage.tsx
@@ -3,60 +3,68 @@ import { NavLink } from "react-router-dom";
 
 const UserPage = () => {
   return (
-    <div className="w-full h-full flex flex-col overflow-y-auto overflow-x-hidden">
+    <div className="w-full h-full flex flex-col gap-6 overflow-y-auto overflow-x-hidden">
       <span className="typo-h2_bold20 text-gray-900 pt-13 px-4">마이페이지</span>
 
-      <div className="flex flex-col items-center gap-4 pt-6 pb-4">
-        <div className="text-gray-800">
-          <span className="typo-body_bold16">홍길동</span>
-          <span className="typo-body_reg16">의 루틴나무</span>
+      <div className="flex flex-col gap-4.5">
+        <div className="flex flex-col items-center gap-4.5">
+          <div className="text-gray-800">
+            <span className="typo-body_bold16 text-gray-800">홍길동</span>
+            <span className="typo-body_reg16 text-gray-800">의 루틴나무</span>
+          </div>
+
+          <div className="min-w-23 min-h-26 bg-gray-400"></div>
+
+          <button className="px-8 py-3 bg-gray-100 text-gray-800 rounded-3xl">
+            <span className="typo-body_bold14 text-gray-800">루틴나무 변경하기</span>
+          </button>
         </div>
 
-        <div className="w-23 h-26 bg-gray-400"></div>
+        <Divider />
 
-        <button className="px-8 py-3 bg-gray-100 text-gray-800 rounded-3xl">
-          <span className="typo-body_bold14">루틴나무 변경하기</span>
-        </button>
-      </div>
-
-      <Divider />
-
-      <div className="w-full px-4 py-5 flex flex-col gap-5">
-        <NavLink to="notice" className="w-full flex justify-between items-center">
-          <span className="typo-body_bold14 text-gray-900">공지사항</span>
-        </NavLink>
-        <NavLink to="inquiry" className="w-full flex justify-between items-center">
-          <span className="typo-body_bold14 text-gray-900">문의하기</span>
-        </NavLink>
-        <NavLink to="info" className="w-full flex justify-between items-center">
-          <span className="typo-body_bold14 text-gray-900">정보</span>
-        </NavLink>
-      </div>
-
-      <Divider />
-
-      <div className="w-full px-4 py-5 flex flex-col gap-5">
-        <NavLink to="account" className="w-full flex justify-between items-center">
-          <span className="typo-body_bold14 text-gray-900">계정 관리</span>
-        </NavLink>
-        <NavLink to="notifications" className="w-full flex justify-between items-center">
-          <span className="typo-body_bold14 text-gray-900">알림 설정</span>
-        </NavLink>
-        <NavLink to="privacy" className="w-full flex justify-between items-center">
-          <span className="typo-body_bold14 text-gray-900">개인정보보호</span>
-        </NavLink>
-        <div className="w-full flex justify-between items-center">
-          <span className="typo-body_bold14 text-gray-900">버전</span>
-          <span className="typo-body_bold14 text-gray-400">1.0.0</span>
+        <div className="w-full px-4 flex flex-col gap-5">
+          <NavLink to="notice" className="w-full flex justify-between items-center">
+            <span className="typo-body_bold14 text-gray-900">공지사항</span>
+            {/* > 아이콘 */}
+          </NavLink>
+          <NavLink to="inquiry" className="w-full flex justify-between items-center">
+            <span className="typo-body_bold14 text-gray-900">문의하기</span>
+            {/* > 아이콘 */}
+          </NavLink>
+          <NavLink to="info" className="w-full flex justify-between items-center">
+            <span className="typo-body_bold14 text-gray-900">정보</span>
+            {/* > 아이콘 */}
+          </NavLink>
         </div>
-        <button
-          onClick={() => {
-            /* 로그아웃 로직 */
-          }}
-          className="w-full flex justify-between items-center"
-        >
-          <span className="typo-body_bold14 text-gray-900">로그아웃</span>
-        </button>
+
+        <Divider />
+
+        <div className="w-full px-4 flex flex-col gap-5">
+          <NavLink to="account" className="w-full flex justify-between items-center">
+            <span className="typo-body_bold14 text-gray-900">계정 관리</span>
+            {/* > 아이콘 */}
+          </NavLink>
+          <NavLink to="notifications" className="w-full flex justify-between items-center">
+            <span className="typo-body_bold14 text-gray-900">알림 설정</span>
+            {/* > 아이콘 */}
+          </NavLink>
+          <NavLink to="privacy" className="w-full flex justify-between items-center">
+            <span className="typo-body_bold14 text-gray-900">개인정보보호</span>
+            {/* > 아이콘 */}
+          </NavLink>
+          <div className="w-full flex justify-between items-center">
+            <span className="typo-body_bold14 text-gray-900">버전</span>
+            <span className="typo-body_bold14 text-gray-400">1.0.0</span>
+          </div>
+          <button
+            onClick={() => {
+              /* 로그아웃 로직 */
+            }}
+            className="w-full flex justify-between items-center"
+          >
+            <span className="typo-body_bold14 text-gray-900">로그아웃</span>
+          </button>
+        </div>
       </div>
     </div>
   );
