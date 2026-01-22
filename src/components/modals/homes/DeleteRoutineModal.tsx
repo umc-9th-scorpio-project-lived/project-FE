40200afac323edfa689cc195ea26d1acb926ff9d
Hyochan <hyochan02@gmail.com>
2026-01-23 01:19:53 +0900

    refactor: 루틴 삭제 모달 삭제 버튼 추가

--- src/components/modals/homes/DeleteRoutineModal.tsx
+++ src/components/modals/homes/DeleteRoutineModal.tsx
@@ -9,9 +9,13 @@ const DeleteRoutineModal = () => {
   const [selected, setSelected] = useState<DeleteOption | null>(null);
 
   const handleSelect = (value: DeleteOption) => {
-    setSelected(value);
-    // closeModal();
+    if (selected === value) {
+      setSelected(null);
+      return;
+    } else setSelected(value);
   };
+
+  const isSelected = selected !== null;
 
   const renderItem = (label: string, value: DeleteOption) => {
     const isSelected = selected === value;
@@ -22,7 +26,7 @@ const DeleteRoutineModal = () => {
         className={`w-full px-4 py-4 rounded-lg typo-body_bold16 transition-colors
           ${
             isSelected
-              ? "bg-primary-50 text-screen-0"
+              ? "bg-primary-10 text-primary-50 border border-primary-50"
               : selected
                 ? "bg-gray-50 text-gray-200"
                 : "bg-gray-50 text-gray-500"
@@ -34,11 +38,23 @@ const DeleteRoutineModal = () => {
   };
 
   return (
-    <div className="bg-white px-4 pt-10 pb-11 rounded-t-2xl">
+    <div className="bg-white px-4 pt-10 pb-11 rounded-t-2xl flex flex-col gap-9">
       <div className="flex flex-col gap-2.5">
         {renderItem("해당 일정 삭제", "CURRENT")}
         {renderItem("이후 일정 삭제", "AFTER")}
         {renderItem("모든 일정 삭제", "ALL")}
+      </div>
+
+      <div
+        role="button"
+        className={`w-full rounded-full typo-body_bold18 py-3 text-center ${isSelected ? "bg-primary-50 text-screen-0" : "bg-gray-100 text-gray-400"}
+        }`}
+        onClick={() => {
+          if (!selected) return;
+          else closeModal();
+        }}
+      >
+        삭제하기
       </div>
     </div>
   );
