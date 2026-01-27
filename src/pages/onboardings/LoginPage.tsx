27b98b9bbed3c33758d7c34509802edbbdad6b95
eun <pdaeun0903@gmail.com>
2026-01-28 01:28:03 +0900

    style: 로그인 페이지 UI 수정

--- src/pages/onboardings/LoginPage.tsx
+++ src/pages/onboardings/LoginPage.tsx
@@ -6,18 +6,18 @@ const LoginPage = () => {
   const navigate = useNavigate();
 
   return (
-    <main className="min-h-dvh font-suite">
+    <main className="min-h-dvh">
       <section className="mx-auto flex min-h-dvh w-full max-w-[500px] flex-col items-center">
         <div className="h-20" />
         {/* 로고 & 타이틀 영역 */}
-        <div className="mt-16 flex flex-col items-center">
+        <div className="mt-16 flex flex-col items-center gap-1">
           <div className="h-[103px] w-[103px] bg-logo" />
-          <div className="mt-3 typo-h1_bold24 text-gray-900">살아보니,</div>
-          <div className="mt-1 typo-body_reg18 text-gray-900">열매가 맺히더라</div>
+          <div className="typo-h1_bold24 text-gray-900">살아보니,</div>
+          <div className="typo-body_reg18 text-gray-900">열매가 맺히더라</div>
         </div>
 
         <div className="flex-1" />
-        <div className="w-full pb-8 flex flex-col items-center">
+        <div className="w-full pb-8 flex flex-col items-center gap-2">
           {/* Kakao Login Button */}
           <button
             type="button"
@@ -26,7 +26,7 @@ const LoginPage = () => {
               flex items-center justify-center w-[290px] h-[58px] gap-[8px] rounded-full bg-[#FFEE50] py-[24px]"
           >
             <KakaoIcon className="size-8" />
-            <span className="typo-body_reg14 font-semibold text-gray-500 whitespace-nowrap">
+            <span className="typo-body_reg14 font-semibold text-gray-400 whitespace-nowrap">
               카카오 계정으로 시작하기
             </span>
           </button>
@@ -34,16 +34,16 @@ const LoginPage = () => {
           {/* Google Login Button */}
           <button
             type="button"
-            className="mt-3 flex items-center justify-center w-[290px] h-[58px] gap-[8px] rounded-full border bg-white py-[24px]"
+            className="flex items-center justify-center w-[290px] h-[58px] gap-2 rounded-full bg-screen-0 border-[0.5px] border-gray-900"
           >
             <GoogleGIcon className="size-6" />
-            <span className="typo-body_reg14 font-semibold text-gray-500 whitespace-nowrap">
+            <span className="typo-body_reg14 font-semibold text-gray-400 whitespace-nowrap">
               구글 계정으로 시작하기
             </span>
           </button>
 
           {/* 약관 안내 문구 */}
-          <p className="mt-4 text-center typo-body_reg12 text-gray-900">
+          <p className="text-center typo-body_reg12 text-gray-900">
             <span className="block">가입 시, 살아보니의</span>
             <span className="whitespace-nowrap">
               이용약관 및 개인정보처리방침에 동의하게 됩니다.
