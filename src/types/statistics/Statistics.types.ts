8349df935e1b56d8d8862f0825846fcddbb373d9
OCJune <cjbormk11@gmail.com>
2026-02-03 16:01:12 +0900

    feat: 열매 팝업 데이터 조회 함수 및 타입 정의 추가

--- src/types/statistics/Statistics.types.ts
+++ src/types/statistics/Statistics.types.ts
@@ -16,7 +16,22 @@ export type FruitsStatistics = {
   fruitList: Fruit[];
 };
 
+export type RoutineAchievement = {
+  title: string;
+  achievementRate: number;
+  memberId: number;
+  year: number;
+  month: number;
+  memberRoutineId: number;
+};
+
 export type YearMonth = {
+  year: number;
+  month: number;
+};
+
+export type RoutineYearMonth = {
+  memberRoutineId: number;
   year: number;
   month: number;
 };
