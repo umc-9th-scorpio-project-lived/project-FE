6ea4cc591e831014d996027c2b427655dbde63f9
eun <pdaeun0903@gmail.com>
2026-02-05 15:34:50 +0900

    feat: 로그아웃 API 연동 및 모달 동작 구현

--- src/components/modals/users/LogoutModal.tsx
+++ src/components/modals/users/LogoutModal.tsx
@@ -1,19 +1,34 @@
-import useBaseModal from "@/stores/modals/baseModal";
+import useBaseModal from '@/stores/modals/baseModal';
+import { useNavigate } from 'react-router-dom';
+import logout from '@/services/auths/logout';
 
 const LogoutModal = () => {
   const { closeModal } = useBaseModal();
+  const navigate = useNavigate();
 
   return (
     <div className="w-fit bg-white rounded-2xl p-5 text-center">
-      <div className="typo-body_bold18 text-gray-900 pt-5 pb-10">로그아웃 하시겠어요?</div>
+      <div className="typo-body_bold18 text-gray-900 pt-5 pb-10">
+        로그아웃 하시겠어요?
+      </div>
 
       <div className="flex justify-center gap-2.5">
-        <button onClick={closeModal} className="w-38 h-14 bg-primary-50 rounded-lg cursor-pointer">
+        <button
+          onClick={closeModal}
+          className="w-38 h-14 bg-primary-50 rounded-lg cursor-pointer"
+        >
           <span className="text-screen-0 typo-body_bold16">취소</span>
         </button>
         <button
-          onClick={() => {
-            /* 로그아웃 로직 */
+          onClick={async () => {
+            try {
+              await logout();
+            } catch (e) {
+            } finally {
+              localStorage.removeItem('accessToken');
+              closeModal();
+              navigate('/', { replace: true });
+            }
           }}
           className="w-38 h-14 bg-gray-100 rounded-lg"
         >
