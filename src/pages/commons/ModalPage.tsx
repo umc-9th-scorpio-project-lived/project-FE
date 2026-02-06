bee125ff10d6ea81364a0f1983d4923f0e33d2b9
유정 <choiyu0wo0d@tukorea.ac.kr>
2026-02-06 17:40:32 +0900

    format: 따옴표 표시방식 변경

--- src/pages/commons/ModalPage.tsx
+++ src/pages/commons/ModalPage.tsx
@@ -1,39 +1,40 @@
-import DeleteRoutineModal from "@/components/modals/homes/DeleteRoutineModal";
-import SelectDateModal from "@/components/modals/homes/SelectDateModal";
-import SelectIconModal from "@/components/modals/homes/SelectIconModal";
-import SetAlarmModal from "@/components/modals/homes/SetAlarmModal";
-import SetRepeatCycleModal from "@/components/modals/homes/SetRepeatCycleModal";
-import LivingYearModal from "@/components/modals/onboardings/LivingYearModal";
-import PushAlarmModal from "@/components/modals/onboardings/PushAlarmModal";
-import ModalBackground from "@/components/modals/ModalBackground";
-import PostDeleteModal from "@/components/modals/communities/PostDeleteModal";
-import ReportPostModal from "@/components/modals/communities/ReportPostModal";
-import SearchDeleteModal from "@/components/modals/communities/SearchDeleteModal";
-import TreeVisibilityModal from "@/components/modals/users/TreeVisibilityModal";
-import UnblockModal from "@/components/modals/users/UnblockModal";
-import useBaseModal from "@/stores/modals/baseModal";
-import SetStatisticsMonthModal from "@/components/modals/trees/SetStatisticsMonthModal";
-import { useEffect } from "react";
-import SetStatisticsWeekModal from "@/components/modals/trees/SetStatisticsWeekModal";
-import SetTrackerMonthModal from "@/components/modals/trees/SetTrackerMonthModal";
-import SetBirthModal from "@/components/modals/onboardings/setBirthModal";
-import FruitInfoModal from "@/components/modals/trees/FruitInfoModal";
-import FruitModal from "@/components/modals/trees/FruitModal";
-import LogoutModal from "@/components/modals/users/LogoutModal";
-import DeleteAccountModal from "@/components/modals/users/DeleteAccountModal";
+import DeleteRoutineModal from '@/components/modals/homes/DeleteRoutineModal';
+import SelectDateModal from '@/components/modals/homes/SelectDateModal';
+import SelectIconModal from '@/components/modals/homes/SelectIconModal';
+import SetAlarmModal from '@/components/modals/homes/SetAlarmModal';
+import SetRepeatCycleModal from '@/components/modals/homes/SetRepeatCycleModal';
+import LivingYearModal from '@/components/modals/onboardings/LivingYearModal';
+import PushAlarmModal from '@/components/modals/onboardings/PushAlarmModal';
+import ModalBackground from '@/components/modals/ModalBackground';
+import PostDeleteModal from '@/components/modals/communities/PostDeleteModal';
+import ReportPostModal from '@/components/modals/communities/ReportPostModal';
+import SearchDeleteModal from '@/components/modals/communities/SearchDeleteModal';
+import TreeVisibilityModal from '@/components/modals/users/TreeVisibilityModal';
+import UnblockModal from '@/components/modals/users/UnblockModal';
+import useBaseModal from '@/stores/modals/baseModal';
+import SetStatisticsMonthModal from '@/components/modals/trees/SetStatisticsMonthModal';
+import { useEffect } from 'react';
+import SetStatisticsWeekModal from '@/components/modals/trees/SetStatisticsWeekModal';
+import SetTrackerMonthModal from '@/components/modals/trees/SetTrackerMonthModal';
+import SetBirthModal from '@/components/modals/onboardings/setBirthModal';
+import FruitInfoModal from '@/components/modals/trees/FruitInfoModal';
+import FruitModal from '@/components/modals/trees/FruitModal';
+import LogoutModal from '@/components/modals/users/LogoutModal';
+import DeleteAccountModal from '@/components/modals/users/DeleteAccountModal';
+import CommentDeleteModal from '@/components/modals/communities/CommentDeleteModal';
 
 const ModalPage = () => {
   const { isModalOpen, modalType, modalProps } = useBaseModal();
 
   useEffect(() => {
     if (isModalOpen) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflow = 'hidden';
     } else {
-      document.body.style.overflow = "";
+      document.body.style.overflow = '';
     }
 
     return () => {
-      document.body.style.overflow = "";
+      document.body.style.overflow = '';
     };
   }, [isModalOpen]);
 
@@ -42,30 +43,43 @@ const ModalPage = () => {
   return (
     <>
       <ModalBackground>
-        {modalType === "selectDateModal" && <SelectDateModal />}
-        {modalType === "setRepeatCycleModal" && <SetRepeatCycleModal {...(modalProps ?? {})} />}
-        {modalType === "setAlarmModal" && <SetAlarmModal {...(modalProps ?? {})} />}
-        {modalType === "selectIconModal" && <SelectIconModal {...(modalProps ?? {})} />}
-        {modalType === "deleteRoutineModal" && <DeleteRoutineModal {...(modalProps ?? {})} />}
-        {modalType === "livingYearModal" && <LivingYearModal />}
-        {modalType === "pushAlarmModal" && <PushAlarmModal />}
-        {modalType === "fruitInfoModal" && <FruitInfoModal />}
-        {modalType === "fruitModal" && <FruitModal />}
-        {modalType === "searchDeleteModal" && <SearchDeleteModal />}
-        {modalType === "reportPostModal" && <ReportPostModal />}
-        {modalType === "postDeleteModal" && <PostDeleteModal />}
-        {modalType === "logoutModal" && <LogoutModal />}
-        {modalType === "deleteAccountModal" && <DeleteAccountModal />}
-        {modalType === "treeVisibilityModal" && <TreeVisibilityModal />}
-        {modalType === "unblockModal" && <UnblockModal />}
-        {modalType === "setStatisticsMonthModal" && (
+        {modalType === 'selectDateModal' && <SelectDateModal />}
+        {modalType === 'setRepeatCycleModal' && (
+          <SetRepeatCycleModal {...(modalProps ?? {})} />
+        )}
+        {modalType === 'setAlarmModal' && (
+          <SetAlarmModal {...(modalProps ?? {})} />
+        )}
+        {modalType === 'selectIconModal' && (
+          <SelectIconModal {...(modalProps ?? {})} />
+        )}
+        {modalType === 'deleteRoutineModal' && (
+          <DeleteRoutineModal {...(modalProps ?? {})} />
+        )}
+        {modalType === 'livingYearModal' && <LivingYearModal />}
+        {modalType === 'pushAlarmModal' && <PushAlarmModal />}
+        {modalType === 'fruitInfoModal' && <FruitInfoModal />}
+        {modalType === 'fruitModal' && <FruitModal />}
+        {modalType === 'searchDeleteModal' && <SearchDeleteModal />}
+        {modalType === 'reportPostModal' && <ReportPostModal />}
+        {modalType === 'postDeleteModal' && <PostDeleteModal />}
+        {modalType === 'commentDeleteModal' && <CommentDeleteModal />}
+        {modalType === 'logoutModal' && <LogoutModal />}
+        {modalType === 'deleteAccountModal' && <DeleteAccountModal />}
+        {modalType === 'treeVisibilityModal' && <TreeVisibilityModal />}
+        {modalType === 'unblockModal' && <UnblockModal />}
+        {modalType === 'setStatisticsMonthModal' && (
           <SetStatisticsMonthModal {...(modalProps ?? {})} />
         )}
-        {modalType === "setStatisticsWeekModal" && (
+        {modalType === 'setStatisticsWeekModal' && (
           <SetStatisticsWeekModal {...(modalProps ?? {})} />
         )}
-        {modalType === "setTrackerMonthModal" && <SetTrackerMonthModal {...(modalProps ?? {})} />}
-        {modalType === "setBirthModal" && <SetBirthModal {...(modalProps ?? {})} />}
+        {modalType === 'setTrackerMonthModal' && (
+          <SetTrackerMonthModal {...(modalProps ?? {})} />
+        )}
+        {modalType === 'setBirthModal' && (
+          <SetBirthModal {...(modalProps ?? {})} />
+        )}
       </ModalBackground>
     </>
   );
