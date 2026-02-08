e6062ac319b7c1f7026a5cb3718ef0af09471f84
유정 <choiyu0wo0d@tukorea.ac.kr>
2026-02-08 21:36:02 +0900

    feat: 게시글 신고 토스트메시지 추가

--- src/components/modals/communities/ReportPostModal.tsx
+++ src/components/modals/communities/ReportPostModal.tsx
@@ -3,20 +3,24 @@ import {
   type ReportReason,
   type ReportTargetType,
 } from '@/constants/report';
-import CloseIcon from '@/icons/CloseIcon';
 import { report } from '@/services/posts/report';
 import useBaseModal from '@/stores/modals/baseModal';
+import useToast from '@/stores/toasts/baseToast';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 type ReportModalProps = {
   targetType: ReportTargetType;
   targetId: number;
+  onSuccess?: () => void;
 };
 
 const ReportPostModal = () => {
   const { closeModal, modalProps } = useBaseModal();
+  const { showToast } = useToast();
+  const navigate = useNavigate();
 
-  const { targetType, targetId } = modalProps as ReportModalProps;
+  const { targetType, targetId, onSuccess } = modalProps as ReportModalProps;
 
   const [selectReason, setSelectReason] = useState<ReportReason | null>(null);
   const [detail, setDetail] = useState('');
@@ -34,6 +38,17 @@ const ReportPostModal = () => {
         detail: detail || null,
       });
       closeModal();
+      showToast(
+        '해당 신고 사유가 접수되었습니다. \n신고된 내용은 검토하여 조치할 예정입니다.',
+        'check'
+      );
+      if (targetType === 'POST') {
+        navigate('/lived/community');
+      }
+      if (targetType === 'COMMENT') {
+        onSuccess?.();
+        return;
+      }
     } catch (e) {
       console.error(e);
     }
@@ -57,9 +72,7 @@ const ReportPostModal = () => {
               <span
                 className={`w-6 h-6 rounded-full flex items-center justify-center ${select ? 'bg-gray-900' : 'bg-gray-200'}`}
               >
-                {select && (
-                  <span className="w-3 h-3 rounded-full bg-screen-0" />
-                )}
+                <span className="w-3 h-3 rounded-full bg-screen-0" />
               </span>
               <span>{content}</span>
             </button>
@@ -69,7 +82,7 @@ const ReportPostModal = () => {
       {/*상세 사유*/}
       {selectReason && (
         <textarea
-          className="w-full h-28 rounded-sm p-2 my-2 border-[0.5px] border-gray-300 text-[12px] resize-none"
+          className="w-full h-28 rounded-sm p-2 my-2 border-[0.5px] border-gray-300 text-[14px] resize-none"
           placeholder="상세 사유를 작성해 주세요.(선택)"
           value={detail}
           onChange={(e) => setDetail(e.target.value)}
